5600805c8e7926540b7b8258c4a9a818860883af
Alvin Ajith <ajithalv@sheridancollege.ca>
2026-05-19 14:14:55 -0400

    student java created

---
+++ src/firstprogram/one/two/Student.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package firstprogram.one.two;
+
+/**
+ *
+ * @author alvin
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public Student(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+    
+}
